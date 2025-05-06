b56c34d3be416a4319271549750cfe63e25dc6c2
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-06 09:22:44 -0500

    finished music branch

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,27 @@
 // import the Media class:
 
+const Media = require('./Media');
+
 // create your Music class:
+
+class Music extends Media {
+    constructor(title, year, genre, artist, length) {
+        super(title, year, genre);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    static shortestAlbum(music){
+        let shortest = Number.POSITIVE_INFINITY;
+        for (let i = 0; i < music.length; i++){
+            if (music[i].length < shortest){
+                shortest = music[i].length;
+                smallestAlbum = music[i];
+            }
+        }
+        return smallestAlbum;
+    }
+}
 
 // don't change below
 module.exports = Music;
