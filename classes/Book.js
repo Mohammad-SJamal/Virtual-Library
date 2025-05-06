16bd5d6033a013586c527fe4ad0c64b5676d5a70
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2025-05-06 09:09:29 -0500

    worked on book class

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,31 @@
 // import the Media class:
 
+const Media = require('./Media');
+
 // create your Book class:
+
+class Book extends Media {
+    constructor(title, year, genre, author, numPages, rating) {
+        super(title, year, genre)
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    static highestRating(books) {
+        let highestRating = 1;
+        for (let i = 0; i < books.length; i++) {
+            if (books[i].rating > highestRating) {
+                highestRating = books[i].rating;
+            }
+        }
+        return highestRating;
+    }
+}
 
 // don't change below
 module.exports = Book;
